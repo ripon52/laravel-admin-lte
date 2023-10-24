c0ed69bfd04a77084d9fb269fa8bfdc4092ba34a
Ripon Uddin <20910478+ripon52@users.noreply.github.com>
2023-10-24 13:31:09 +0600

    mastering complete

---
+++ resources/views/admin/includes/footer-script.blade.php
@@ -0,0 +1,17 @@
+
+<!-- REQUIRED SCRIPTS -->
+<!-- jQuery -->
+<script src="{{ asset('dashboardFiles/js/jquery.min.js') }}"></script>
+<!-- Bootstrap -->
+<script src="{{ asset('dashboardFiles/js/bootstrap.bundle.min.js') }}"></script>
+<!-- overlayScrollbars -->
+<script src="{{ asset('dashboardFiles/js/jquery.overlayScrollbars.min.js') }}"></script>
+<!-- AdminLTE App -->
+<script src="{{ asset('dashboardFiles/js/adminlte.js') }}"></script>
+
+<!-- PAGE PLUGINS -->
+
+<!-- AdminLTE for demo purposes -->
+<script src="{{ asset('dashboardFiles/js/demo.js') }}"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="{{ asset('dashboardFiles/js/dashboard2.js') }}"></script>
---
+++ resources/views/admin/layouts/admin.blade.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>@yield("title") | Laravel Dashboard</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome Icons -->
+  <link rel="stylesheet" href="{{ asset('dashboardFiles/css') }}/all.min.css">
+  <!-- overlayScrollbars -->
+  <link rel="stylesheet" href="{{ asset('dashboardFiles/css') }}/OverlayScrollbars.min.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+  <!-- Theme style -->
+  <link rel="stylesheet" href="{{ asset('dashboardFiles/css') }}/adminlte.min.css">
+
+    @yield("css")
+    @yield("styles")
+    @yield("links")
+
+</head>
+<body class="hold-transition dark-mode sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
+
+    <div class="wrapper">
+
+      <!-- Preloader -->
+      <div class="preloader flex-column justify-content-center align-items-center">
+        <img class="animation__wobble" src="{{ asset('dashboardFiles/images') }}/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+      </div>
+
+        {{-- Dashboard Header --}}
+        @include("admin.includes.header")
+      <!-- Main Sidebar Container -->
+
+        {{-- Dashboard Sidebar Start --}}
+        @include("admin.includes.admin-sidebar")
+        {{-- Dashboard Sidebar End --}}
+
+          <!-- Content Wrapper. Contains page content -->
+          <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            @include("admin.includes.page-header")
+            <!-- /.content-header -->
+
+            <!-- Main content -->
+            <section class="content">
+              <div class="container-fluid">
+                  @yield("content")
+
+              </div><!--/. container-fluid -->
+            </section>
+            <!-- /.content -->
+          </div>
+         <!-- /.content-wrapper -->
+
+          <!-- Control Sidebar -->
+          <aside class="control-sidebar control-sidebar-dark">
+            <!-- Control sidebar content goes here -->
+          </aside>
+            <!-- /.control-sidebar -->
+        <!-- Main Footer -->
+        @include("admin.includes.footer")
+    </div>
+    <!-- ./wrapper -->
+    @include("admin.includes.footer-script")
+
+
+    @yield("js")
+    @yield("scripts")
+</body>
+</html>
